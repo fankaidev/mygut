c6a98cb14fce4d9d358b0e9aef4002cf94ef3b7f
FAN Kai <fankaidev@gmail.com>
2026-04-12 06:35:57 +0800

    Revert "feat: implement medical record feature (#8) (#24)"
    This reverts commit cd18e04813bd7e3003f4be0d6eeb20f6174fe7ea.

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,8 +7,6 @@ export default defineAppConfig({
     "pages/meal/add/index",
     "pages/stool/index/index",
     "pages/stool/add/index",
-    "pages/medical/index/index",
-    "pages/medical/add/index",
   ],
   window: {
     backgroundTextStyle: "light",
